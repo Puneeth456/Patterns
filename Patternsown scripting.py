ec7c7c0eab9168f29e0e1f759dd99786852cbf74
puneeth456 <shivanpuneeth50@gmail.com>
2019-06-23 16:24:00 +0530

    first push

---
+++ Patternsown scripting.py
@@ -0,0 +1,18 @@
+for i in range(4):
+    for j in range(4-i):
+        print("#",end="")
+    print()
+#
+# for j in range(4):
+#     print("#",end="")
+#
+# print()
+#
+# for j in range(4):
+#     print("#",end="")
+#
+# print()
+#
+# for j in range(4):
+#     print("#",end="")
+
